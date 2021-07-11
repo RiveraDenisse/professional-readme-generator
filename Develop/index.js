d25782305ae8a1fe1e735fec32a838fca779a2fb
Denisse Rivera <denisse_alejandrar@hotmail.com>
2021-07-11 16:05:28 -0500

    added function to add license links based on users input

--- Develop/index.js
+++ Develop/index.js
@@ -1,4 +1,4 @@
-// TODO: Include packages needed for this application
+//packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
 //to export generateMarkdown function
@@ -8,154 +8,152 @@ const generateMarkdown = require('./utils/generateMarkdown');
 //Some questions will require to validation
 const questions = [
     { 
-        type: 'input',
-        name: 'title',
-        message: 'What is the name of your project? (Required)',
-        validate: titleInput => {
-            if (titleInput) {
-                return true;
-              } else {
-                console.log('You need to enter a project name');
-                return false;
-              }
+      type: 'input',
+      name: 'title',
+      message: 'What is the name of your project? (Required)',
+      validate: titleInput => {
+        if (titleInput) {
+          return true;
+        } else {
+          console.log('You need to enter a project name');
+          return false;
         }
+      }
     },
     {
-        type: 'input',
-        name: 'Description',
-        message: 'Provide a description of the project (Required)',
-        validate: descriptionInput => {
-          if (descriptionInput) {
-            return true;
-          } else {
-            console.log('You need to enter a project description');
-            return false;
-          }
+      type: 'input',
+      name: 'Description',
+      message: 'Provide a description of the project (Required)',
+      validate: descriptionInput => {
+        if (descriptionInput) {
+          return true;
+        } else {
+          console.log('You need to enter a project description');
+          return false;
         }
+      }
     },
     {
-        type: 'input',
-        name: 'Installation',
-        message: 'Please provide all steps required to install your project[Provide step-by-step description] (Required)',
-        validate: InstallationInput => {
-          if (InstallationInput) {
-            return true;
-          } else {
-            console.log('You need to enter the steps required to install your project');
-            return false;
-          }
-        }   
+      type: 'input',
+      name: 'Installation',
+      message: 'Please provide all steps required to install your project[Provide step-by-step description] (Required)',
+      validate: InstallationInput => {
+        if (InstallationInput) {
+          return true;
+        } else {
+          console.log('You need to enter the steps required to install your project');
+          return false;
+        }
+      }   
     },
     {
-        type: 'input',
-        name: 'Usage',
-        message: 'Please provide instructions on how to use your project (Required)',
-        validate: UsageInput => {
-          if (UsageInput) {
-            return true;
-          } else {
-            console.log('You need to enter instructions on how to use the project(or examples)');
-            return false;
-          }
-        }   
+      type: 'input',
+      name: 'Usage',
+      message: 'Please provide instructions on how to use your project (Required)',
+      validate: UsageInput => {
+        if (UsageInput) {
+          return true;
+        } else {
+          console.log('You need to enter instructions on how to use the project(or examples)');
+          return false;
+        }
+      }   
     },
     //here goes the question for licensing (if user chooses no license then it will not ask to choose a license)
     {
-        type: 'confirm',
-        name: 'confirmLicense',
-        message: 'Would you like to add a license to your project? (this will create a license badge as well)',
+      type: 'confirm',
+      name: 'confirmLicense',
+      message: 'Would you like to add a license to your project? (this will create a license badge as well)',
     },
     //if user types Y to add instructions then the question below will be prompted
     {
-        type: 'list',
-        name: 'License',
-        message: "Please choose a license to your project from the list below",
-        when: (data) => data.confirmLicense === true,
-        choices: ['MIT', 'Apache2.0', 'GNUGPLv3', 'MozillaPublicLicense2.0', 'TheUnlicense']
+      type: 'list',
+      name: 'License',
+      message: "Please choose a license to your project from the list below",
+      when: (data) => data.confirmLicense === true,
+      choices: ['MIT', 'Apache2.0', 'GNUGPLv3', 'MozillaPublicLicense2.0', 'TheUnlicense']
     },
     {
-        type: 'input',
-        name: 'Contributing',
-        message: "Please add instructions for contributing to your project",
-        validate: ContributingInput => {
-          if (ContributingInput) {
-            return true;
-          } else {
-            console.log('Please add instructions on how to collaborate');
-            return false;
-          }
-        }   
-    },
-    {
-        type: 'input',
-        name: 'Testing',
-        message: "Please add testing instructions/examples?",
-        validate: TestingInput => {
-          if (TestingInput) {
-            return true;
-          } else {
-            console.log('Please add instructions on how to collaborate');
-            return false;
-          }
+      type: 'input',
+      name: 'Contributing',
+      message: "Please add instructions for contributing to your project",
+      validate: ContributingInput => {
+        if (ContributingInput) {
+          return true;
+        } else {
+          console.log('Please add instructions on how to collaborate');
+          return false;
         }
+      }   
     },
     {
-        type: 'input',
-        name: 'Credits',
-        message: "Please list the contributors",
-        validate: CreditsInput => {
-          if (CreditsInput) {
-            return true;
-          } else {
-            console.log('Please list your contributors');
-            return false;
-          }
+      type: 'input',
+      name: 'Testing',
+      message: "Please add testing instructions/examples?",
+      validate: TestingInput => {
+        if (TestingInput) {
+          return true;
+        } else {
+          console.log('Please add instructions on how to collaborate');
+          return false;
         }
-    },
-    //Questions section
-    {
-        type: 'input',
-        name: 'username',
-        message: 'Please provide your github username',
-        validate: usernameInput => {
-            if (usernameInput) {
-                return true;
-            } else {
-                console.log('Please provide a username');
-                return false;
-            }
-        }
+      }
     },
     {
-        type: 'input',
-        name: 'email',
-        message: 'Please provide your an email to contact you',
-        validate: emailInput => {
-            if (emailInput) {
-                return true;
-            } else {
-                console.log('Please provide a email');
-                return false;
-            }
+      type: 'input',
+      name: 'Credits',
+      message: "Please list the contributors",
+      validate: CreditsInput => {
+        if (CreditsInput) {
+          return true;
+        } else {
+          console.log('Please list your contributors');
+          return false;
         }
+      }
+    },
+    {
+      type: 'input',
+      name: 'username',
+      message: 'Please provide your github username',
+      validate: usernameInput => {
+        if (usernameInput) {
+          return true;
+        } else {
+          console.log('Please provide a username');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Please provide your an email to contact you',
+      validate: emailInput => {
+        if (emailInput) {
+          return true;
+        } else {
+          console.log('Please provide a email');
+          return false;
+        }
+      }
     }      
-
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName,data) {
-    fs.writeFile(fileName, data, err=>{
-        if (err){
-            return console.log(err);
-        }
-        console.log("Your README.md file was generated")
-    });
+  fs.writeFile(fileName, data, err=>{
+    if (err){
+      return console.log(err);
+    }
+    console.log("Your README.md file was generated")
+  });
 }
 
 // TODO: Create a function to initialize app, prompt will pass the question array, from there the userfeedback will be used to create the README
 function init() {
-    inquirer.prompt(questions)
-    .then((data)=>writeToFile('GENERATEDREADME.md',generateMarkdown(data)))
+  inquirer.prompt(questions)
+  .then((data)=>writeToFile('GENERATEDREADME.md',generateMarkdown(data)))
 }
 
 // Function call to initialize app
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -33,18 +33,37 @@ const renderTableofContents = License => {
   `;
 };
 
-// If there is no license, return an empty string
-const renderLicenseSection = License => {
-  if (!License){
+// If there is no license it will return an empty string, if user selects a license then license section will populate with the license information and link
+function renderLicenseSection(License) {
+  let MIT = `## License
+  This application is covered by MIT License. For more information about the license please click [here](https://choosealicense.com/licenses/mit/)`;
+  let Apache = `## License
+  This application is covered by Apache License 2.0. For more information about the license please click [here](https://choosealicense.com/licenses/apache-2.0/)`;
+  let GNU = `## License
+  This application is covered by GNU General Public License v3.0. For more information about the license please click [here](https://choosealicense.com/licenses/gpl-3.0/)`;
+  let Mozilla = `## License
+  This application is covered by Mozilla Public License 2.0. For more information about the license please click [here](https://choosealicense.com/licenses/mpl-2.0/)`;
+  let Unlicense = `## License
+  This application is covered by The Unlicense. For more information about the license please click [here](https://choosealicense.com/licenses/unlicense/)`;
+  if (License ==='MIT') {
+    return MIT;
+  }
+  else if (License === 'Apache2.0') {
+    return Apache;
+  }
+  else if (License === 'GNUGPLv3') {
+    return GNU;
+  }
+  else if (License === 'MozillaPublicLicense2.0') {
+    return Mozilla;
+  }
+  else if (License === 'TheUnlicense') {
+    return Unlicense;
+  }
+  else {
     return '';
   }
-  return `
-  ## License
-
-  ${License}
-`;
 }
-
 //this is the template for the README file
 function generateMarkdown(data) {
   return `# ${data.title}
